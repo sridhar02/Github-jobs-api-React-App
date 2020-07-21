091ea1da1b60e985494c2a70ff478b37c81e9bd4
sridhar02 <kattasridhar02@gmail.com>
2020-07-21 18:46:35 +0530

    Updates

---
+++ precache-manifest.0e1ae3d22c7c496c2d9b6ad2a8ffa03e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "903c2533e57afa8e23c299673eef2ed7",
+    "url": "/Github-jobs-api-React-App/index.html"
+  },
+  {
+    "revision": "f1122f5e758d4ad5859f",
+    "url": "/Github-jobs-api-React-App/static/js/2.09175c33.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/Github-jobs-api-React-App/static/js/2.09175c33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0737c0fe20897bd99713",
+    "url": "/Github-jobs-api-React-App/static/js/main.00abcc09.chunk.js"
+  },
+  {
+    "revision": "567bb96a0573f146b627",
+    "url": "/Github-jobs-api-React-App/static/js/runtime-main.8d5e587c.js"
+  }
+]);
